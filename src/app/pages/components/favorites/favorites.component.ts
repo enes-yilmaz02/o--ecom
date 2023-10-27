839fa8f7c446e9ff06c4379d6589634fc2b43454
enes yilmaz <enesyilmaz@eness-MacBook-Air.local>
2023-10-27 15:21:14 +0300

    add firebase in cart service

--- src/app/pages/components/favorites/favorites.component.ts
+++ src/app/pages/components/favorites/favorites.component.ts
@@ -5,25 +5,22 @@ import { CartService } from 'src/app/services/cart.service';
 @Component({
   selector: 'app-favorites',
   templateUrl: './favorites.component.html',
-  styleUrls: ['./favorites.component.scss']
+  styleUrls: ['./favorites.component.scss'],
 })
 export class FavoritesComponent {
   items = this.cart.getItemsFavorites();
+  constructor(
+    private cart: CartService,
+    private messageService: MessageService
+  ) {}
 
-  constructor(private cart:CartService , private messageService:MessageService){}
-
-removeFromCartFavorites(item) {
-  // Sepetten ürünü kaldırma işlemi
-  // Örnek: CartService kullanarak sepetten ürünü kaldırmak
-  this.cart.removeFromCartFavorites(item);
-  this.messageService.add({
-    severity: 'success',
-    summary: 'Başarılı',
-    detail: 'Seçilen ürün favorilerimden kaldırıldı',
-  });
-  // Ürünü kaldırdıktan sonra items dizisini güncelleyin
-  this.items = this.cart.getItemsFavorites();
-
-}
-
+  removeFromCartFavorites(item) {
+    this.cart.removeFromCartFavorites(item);
+    this.messageService.add({
+      severity: 'success',
+      summary: 'Başarılı',
+      detail: 'Seçilen ürün favorilerimden kaldırıldı',
+    });
+    this.items = this.cart.getItemsFavorites();
+  }
 }
--- src/app/services/cart.service.ts
+++ src/app/services/cart.service.ts
@@ -1,45 +1,75 @@
 import { Injectable } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { MessageService } from 'primeng/api';
+import { Product } from '../models/product';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CartService {
-  private cartItems = new BehaviorSubject<any[]>([]);
-  private cartItemsFavorites = new BehaviorSubject<any[]>([]);
-  cartItems$ = this.cartItems.asObservable();
-  cartItemsFavorites$= this.cartItemsFavorites.asObservable();
-
-  constructor() { }
+  private cartItems = new Observable<Product[]>();
+  private cartItemsFavorites = new Observable<Product[]>();
+  private collectionName ='orders';
+  private collectionNameFavorites='favorites';
+  constructor(private afs: AngularFirestore,private messageService:MessageService) {
+    console.log(this.getItems())
+  }
 
   addToCart(item: any) {
-    const currentItems = this.cartItems.getValue();
-    currentItems.push(item);
-    this.cartItems.next(currentItems);
+    return this.afs.collection(this.collectionName).doc().set({
+      item
+    }).then(()=>{
+      this.messageService.add({
+        severity: 'succes',
+        summary: 'Başarılı!',
+        detail:
+          'Ürün sepete eklendiii',
+      });
+    }).catch((error) => {
+    this.messageService.add({
+      severity: 'warn',
+      summary: 'Hata!',
+      detail:
+        'Beklenmeyen bir hata oluştu. Lütfen tekrar deneyin',
+    });
+  });
   }
 
   addToCartFavorites(item: any) {
-    const currentItems = this.cartItemsFavorites.getValue();
-    currentItems.push(item);
-    this.cartItemsFavorites.next(currentItems);
+
+    return this.afs.collection(this.collectionNameFavorites).doc().set({
+      item
+    }).then(()=>{
+      this.messageService.add({
+        severity: 'succes',
+        summary: 'Başarılı!',
+        detail:
+          'Ürün sepete eklendiii',
+      });
+    }).catch((error) => {
+    this.messageService.add({
+      severity: 'warn',
+      summary: 'Hata!',
+      detail:
+        'Beklenmeyen bir hata oluştu. Lütfen tekrar deneyin',
+    });
+  });
   }
 
-  removeFromCart(item: any) {
-    const currentItems = this.cartItems.getValue();
-    const updatedItems = currentItems.filter(cartItem => cartItem !== item);
-    this.cartItems.next(updatedItems);
+  removeFromCart(item) {
+    return this.afs.collection(this.collectionName).doc(item).delete();
   }
 
   removeFromCartFavorites(item: any) {
-    const currentItems = this.cartItemsFavorites.getValue();
-    const updatedItems = currentItems.filter(cartItemsFavorites => cartItemsFavorites !== item);
-    this.cartItemsFavorites.next(updatedItems);
-  }
-  getItemsFavorites() {
-    return this.cartItemsFavorites.getValue();
+    return this.afs.collection(this.collectionNameFavorites).doc(item).delete();
   }
 
-  getItems() {
-    return this.cartItems.getValue();
+  getItemsFavorites() {
+    return this.afs.collection(this.collectionNameFavorites).valueChanges();
+  }
+
+  getItems(){
+    return this.afs.collection(this.collectionName).valueChanges();
   }
 }
