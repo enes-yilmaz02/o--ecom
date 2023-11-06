f94a5129b48b13918ada51ef3b63b9fb48d7e248
enes-yilmaz02 <ey684241@email.com>
2023-11-06 15:00:52 +0300

    add admin module

--- src/app/admin/admin-routing.module.ts
+++ src/app/admin/admin-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdminComponent } from './admin.component';
 
-const routes: Routes = [];
+
 
 @NgModule({
-  imports: [RouterModule.forChild(routes)],
+  imports: [RouterModule.forChild([
+    {
+      path:'' , component:AdminComponent ,
+      children:[
+        //{path:'',redirectTo:'dashboard' },
+      ]
+    }
+  ])],
   exports: [RouterModule]
 })
 export class AdminRoutingModule { }
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,14 +2,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-
   {
     path: '',
     loadChildren: () =>
-                    import('./pages/pages.module').then(
-                        (m) => m.PagesModule
-                    ),
-
+      import('./pages/pages.module').then((m) => m.PagesModule),
+  },
+  {
+    path: 'admin',
+    loadChildren: () =>
+      import('./admin/admin.module').then((m) => m.AdminModule),
   },
 ];
 
