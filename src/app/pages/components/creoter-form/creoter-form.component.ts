49792761d60cfa032721d10b996645e4ed3353ef
enes-yilmaz02 <ey684241@email.com>
2023-11-17 15:20:29 +0300

    add creoter form

--- src/app/pages/components/creoter-form/creoter-form.component.ts
+++ src/app/pages/components/creoter-form/creoter-form.component.ts
@@ -1,7 +1,7 @@
 import { UserService } from 'src/app/services/user.service';
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { BehaviorSubject, Observable, tap } from 'rxjs';
+import { BehaviorSubject, EMPTY, Observable, map, switchMap, tap } from 'rxjs';
 import { UserRole } from 'src/app/models/role.enum';
 import { MessageService } from 'primeng/api';
 import { Router } from '@angular/router';
@@ -34,7 +34,7 @@ export class CreoterFormComponent {
       companyName: ['', Validators.required],
       taxNumber: ['', [Validators.required, Validators.minLength(8)]],
       email: ['', [Validators.required, Validators.email]],
-      code:['',Validators.required]
+      code:['']
     });
   }
 
@@ -46,29 +46,58 @@ export class CreoterFormComponent {
     );
   }
 
+  checkEmailAvailability(email: string): Observable<{ available: boolean }> {
+    return this.userService.getUsers().pipe(
+      map((users) => {
+        const userWithEmail = users.find((user) => user.email === email);
+        return { available: !userWithEmail }; // Eğer userWithEmail değeri varsa e-posta adresi kullanılmaktadır.
+      })
+    );
+  }
+
   onSubmit() {
-     this.generatedCode = this.generateRandomCode();
-    const body = {
-      to: this.creoterForm.get('email').value,
-      subject: 'email doğrulama',
-      text: 'email doğrulama kodunuz: ' + this.generatedCode,
-    };
-    this.getUserId().subscribe((userId: any) => {
-      this.userId = userId;
-      this.userService.sendEmail(this.userId, body).subscribe(
-        (response) => {
-         this.senderMail.next(true);
-         this.messageService.add({
-          severity:'success',
-          summary:'Başarılı!',
-          detail:"E-posta adresinize gönderilen doğrulama kodu ile hesabınızı doğrulayabilirsiniz"
-         })
-        },
-        (error) => {
-          console.error('Error sending email', error);
+    if (this.creoterForm.valid) {
+      this.generatedCode = this.generateRandomCode();
+      const email = this.creoterForm.get('email').value;
+
+      this.checkEmailAvailability(email).subscribe(result => {
+        if (result && result.available) {
+          const body = {
+            to: email,
+            subject: 'email doğrulama',
+            text: 'email doğrulama kodunuz: ' + this.generatedCode,
+          };
+          this.getUserId().subscribe(userId => {
+            this.userId = userId;
+            this.userService.sendEmail(this.userId, body).subscribe(
+              () => {
+                this.senderMail.next(true);
+                this.messageService.add({
+                  severity: 'success',
+                  summary: 'Başarılı!',
+                  detail: 'E-posta adresinize gönderilen doğrulama kodu ile hesabınızı doğrulayabilirsiniz'
+                });
+              },
+              error => {
+                console.error('Error sending email', error);
+              }
+            );
+          });
+        }else{
+          this.messageService.add({
+            severity: 'error',
+            summary: 'Hata',
+            detail: 'Bu e-posta adresi zaten kullanılmaktadır. Lütfen başka bir e-posta adresi seçin.',
+          });
         }
-      );
-    });
+      });
+    } else {
+      this.messageService.add({
+        severity: 'warn',
+        summary: 'Lütfen!',
+        detail: 'Form bilgilerini doldurunuz..'
+      });
+    }
   }
 
   generateRandomCode(): string {
@@ -84,7 +113,6 @@ export class CreoterFormComponent {
   }
 
   verifyCode(){
-debugger
     const code = this.creoterForm.get('code').value;
      if(code===this.generatedCode){
       this.getUserId().subscribe(()=>{
@@ -95,15 +123,21 @@ debugger
           this.messageService.add({
             severity:'success',
             summary:'Başarılı',
-            detail:"Kullanıcı bilgileri güncellendi"
+            detail:"Artık bir satıcısınız... Şimdi giriş sayfasına yönlendiriliyorsunuz"
           });
-          localStorage.clear();
-          this.router.navigate(['/login']);
+          setTimeout(() => {
+            localStorage.clear();
+            this.router.navigate(['/login']);
+          }, 3000);
         });
       })
      }
      else{
-      alert('yanlış');
+      this.messageService.add({
+        severity:'warn',
+        summary:'Doğrulama kodu',
+        detail:"Girdiğiniz kod doğrulanamadı... Lütfen tekrar deneyiniz"
+      });
      }
   }
 }
