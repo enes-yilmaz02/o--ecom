a81c30895571258ebef7a02c108e2daad981b419
enes-yilmaz02 <ey684241@email.com>
2023-12-06 15:12:11 +0300

    notfound done

--- src/app/notfound/notfound.module.ts
+++ src/app/notfound/notfound.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NotfoundComponent } from './notfound.component';
 import { SharedModule } from '../shared/shared.module';
-
+import { NotfoundRoutingModule } from './notfound-routing.module';
 
 @NgModule({
   declarations: [
@@ -10,7 +10,11 @@ import { SharedModule } from '../shared/shared.module';
   ],
   imports: [
     CommonModule,
-    SharedModule
+    SharedModule,
+    NotfoundRoutingModule
+  ],
+  exports:[
+    NotfoundComponent
   ]
 })
 export class NotfoundModule { }
