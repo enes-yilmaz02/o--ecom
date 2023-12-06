5c180d1044719c88f4f305c92da2eed39fb42493
enes-yilmaz02 <ey684241@email.com>
2023-12-06 18:57:16 +0300

    creoterForm fixed

--- src/app/pages/components/creoter-form/creoter-form.component.ts
+++ src/app/pages/components/creoter-form/creoter-form.component.ts
@@ -8,41 +8,40 @@ import { Router } from '@angular/router';
 import { CitiesService } from 'src/app/services/cities.service';
 import { CereoterService } from 'src/app/services/cereoter.service';
 
-
 @Component({
   selector: 'app-creoter-form',
   templateUrl: './creoter-form.component.html',
   styleUrls: ['./creoter-form.component.scss'],
 })
 export class CreoterFormComponent implements OnInit {
-
   creoterForm: FormGroup;
 
   userId: any;
 
-  generatedCode:any;
+  generatedCode: any;
 
   senderMail: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
 
   cities: any[] = [];
   districts: any[] = [];
   selectedCity: any | undefined;
+  userEmail: any;
+  userData: any;
 
   constructor(
     private formBuilder: FormBuilder,
     private userService: UserService,
-    private messageService:MessageService,
-    private router:Router,
-    private cityService:CitiesService,
-    private creoterService:CereoterService
-
+    private messageService: MessageService,
+    private router: Router,
+    private cityService: CitiesService,
+    private creoterService: CereoterService
   ) {
     this.creoterForm = this.formBuilder.group({
       companyName: ['', [Validators.required]],
       taxNumber: ['', [Validators.required]],
       email: ['', [Validators.required, Validators.email]],
       city: ['', Validators.required],
-      discrits: [''],
+      distcrits: [''],
       code: [''],
     });
   }
@@ -57,11 +56,12 @@ export class CreoterFormComponent implements OnInit {
       this.selectedCity = null; // Varsayılan olarak seçili şehir null olsun
 
       // Sehir seçildiğinde ilçeleri güncelle
-      this.onCityChange({ value: this.selectedCity});
-     
+      this.onCityChange({ value: this.selectedCity });
     });
+
+    this.getUserData();
   }
-  
+
   // noWhitespaceValidator(control: { value: string }): null | { whitespace: boolean } {
   //   const isWhitespace = (control.value || '').trim().length === 0;
   //   return isWhitespace ? { whitespace: true } : null;
@@ -70,9 +70,11 @@ export class CreoterFormComponent implements OnInit {
   onCityChange(event: any): void {
     if (event && event.value) {
       const selectedCity = event.value;
-  
+
       // Seçilen şehir varsa ilçeleri güncelle
-      this.districts = selectedCity.discrits.map((district: string) => ({ label: district }));
+      this.districts = selectedCity.discrits.map((district: string) => ({
+        label: district,
+      }));
     } else {
       // Seçilen şehir yoksa ilçeleri temizle
       this.districts = [];
@@ -87,49 +89,58 @@ export class CreoterFormComponent implements OnInit {
     );
   }
 
-  checkEmailAvailability(email: string): Observable<{ available: boolean }> {
-    return this.userService.getUsers().pipe(
-      map((users) => {
-        const userWithEmail = users.find((user) => user.email === email);
-        return { available: !userWithEmail }; // Eğer userWithEmail değeri varsa e-posta adresi kullanılmaktadır.
-      })
-    );
+  // checkEmailAvailability(email: string): Observable<{ available: boolean }> {
+  //   return this.creoterService.getCreoter().pipe(
+  //     map((users) => {
+  //       const userWithEmail = users.find((user) => user.email === email);
+  //       return { available: !userWithEmail }; // Eğer userWithEmail değeri varsa e-posta adresi kullanılmaktadır.
+  //     })
+  //   );
+  // }
+
+  getUserData() {
+    this.getUserId().subscribe(() => {
+      this.userService.getUser(this.userId).subscribe((data: any) => {
+        this.userData = data;
+        this.userEmail = data.email;
+        this.creoterForm.patchValue({
+          email: this.userEmail,
+        });
+      });
+    });
   }
 
   onSubmit() {
     if (this.creoterForm.valid) {
-      const email = this.creoterForm.get('email').value;
-      this.checkEmailAvailability(email).subscribe(result => {
-        if (result && result.available) {
-          this.getUserId().subscribe(userId => {
-              this.userId = userId;
-              const formArray= this.creoterForm.value;
-              formArray.userId=this.userId;
-              this.creoterService.addCreoter(formArray).subscribe(()=>{
-                this.messageService.add({
-                  severity:'success',
-                  summary:'Başarılı',
-                  detail:"Satıcı başvurunuz Fromunuz gönderildi... Onaylanınca sizi bilgilendireceğiz."
-                });
-                setTimeout(() => {
-                  localStorage.clear();
-                  this.router.navigate(['/pages']);
-                }, 3000);
-              });
-            })
-        }else{
+      this.getUserId().subscribe((userId) => {
+        this.userId = userId;
+        const formArray = this.creoterForm.value;
+        const userData = {
+          userId: this.userId,
+          ...this.userData,
+          companyName: formArray.companyName,
+          taxNumber: formArray.taxNumber,
+          city: formArray.city.label,
+          distcrits: formArray.distcrits.label,
+        };
+        console.log(userData)
+        this.creoterService.addCreoter(userData).subscribe(() => {
           this.messageService.add({
-            severity: 'error',
-            summary: 'Hata',
-            detail: 'Bu e-posta adresi zaten kullanılmaktadır. Lütfen başka bir e-posta adresi seçin.',
+            severity: 'success',
+            summary: 'Başarılı',
+            detail:
+              'Satıcı başvurunuz Fromunuz gönderildi... Onaylanınca sizi bilgilendireceğiz.',
           });
-        }
+          setTimeout(() => {
+            this.router.navigate(['/pages']);
+          }, 3000);
+        });
       });
     } else {
       this.messageService.add({
         severity: 'warn',
         summary: 'Lütfen!',
-        detail: 'Form bilgilerini doldurunuz..'
+        detail: 'Form bilgilerini doldurunuz..',
       });
     }
   }
@@ -145,6 +156,4 @@ export class CreoterFormComponent implements OnInit {
 
     return result;
   }
-
- 
 }
