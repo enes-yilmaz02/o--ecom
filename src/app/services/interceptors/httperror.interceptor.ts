cde4621314039d7bc3dfa0b21d7a2ab969b6817e
enes-yilmaz02 <ey684241@email.com>
2024-01-03 15:39:37 +0300

    http error interceptor

--- src/app/services/interceptors/httperror.interceptor.ts
+++ src/app/services/interceptors/httperror.interceptor.ts
@@ -4,25 +4,27 @@ import {
   HttpHandler,
   HttpEvent,
   HttpInterceptor,
-  HttpErrorResponse
+  HttpErrorResponse,
 } from '@angular/common/http';
 import { Observable, catchError, throwError } from 'rxjs';
 import { Router } from '@angular/router';
 
 @Injectable()
 export class HttperrorInterceptor implements HttpInterceptor {
+  constructor(private router: Router) {}
 
-  constructor(private router:Router) {}
-
-  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-
+  intercept(
+    request: HttpRequest<any>,
+    next: HttpHandler
+  ): Observable<HttpEvent<any>> {
     return next.handle(request).pipe(
       catchError((error: HttpErrorResponse) => {
-
         if (error.status === 0 || error.status === 500) {
-          this.router.navigate(['server-error'])
-      }
-
+          this.router.navigate(['server-error']);
+        }
+        if (error.status === 400 || error.status === 404) {
+          this.router.navigate(['not-found']);
+        }
         return throwError(error);
       })
     );
